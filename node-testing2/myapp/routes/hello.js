7940b9a853d43e876e55f7ff4a403e01eb5857e2
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-02-13 16:27:59 +0200

    adding node-dev watcher

--- node-testing2/myapp/routes/hello.js
+++ node-testing2/myapp/routes/hello.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { msg: "Greeting page!", name: "Johny"});
+  console.log("I am going to send the response by the next routing function");
+  next();
+}, function (req, res, next) {
+  res.render('hello', { msg: "Greeting page!", name: "mr. Who"});
 });
 
 module.exports = router;
