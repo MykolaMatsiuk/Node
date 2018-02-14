dddc1a1d4f5b4918e0617531cd8f2d54d54654e4
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-02-14 16:22:05 +0200

    add return next()

--- node-testing2/myapp/routes/hello.js
+++ node-testing2/myapp/routes/hello.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("I am going to send the response by the next routing function");
-  next();
+  return next();
 }, function (req, res, next) {
   res.render('hello', { msg: "Greeting page!", name: "mr. Who"});
 });
